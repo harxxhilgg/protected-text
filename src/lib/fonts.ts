3c2e742fcbaa2770a70600ed2c65ec8d53dc392e
Harshil Patel <harshilp046@gmail.com>
2026-07-07 09:13:37 +0530

    refactor: add new fonts

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,4 +1,10 @@
-import { Geist, Geist_Mono, Hanken_Grotesk } from "next/font/google";
+import {
+  Geist,
+  Geist_Mono,
+  Hanken_Grotesk,
+  Space_Mono,
+  PT_Sans,
+} from "next/font/google";
 
 export const geist = Geist({
   subsets: ["latin"],
@@ -15,5 +21,17 @@ export const geistMono = Geist_Mono({
 export const hanken = Hanken_Grotesk({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  style: ["normal", "italic"],
+});
+
+export const spaceMono = Space_Mono({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  style: ["normal", "italic"],
+});
+
+export const ptSans = PT_Sans({
+  subsets: ["latin"],
+  weight: ["400", "700"],
   style: ["normal", "italic"],
 });
